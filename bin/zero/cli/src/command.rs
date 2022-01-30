0d4d929d0f7df3de1a38d0737df384552dc0c34f
2075 <marco@zero.io>
2022-01-30 14:58:05 +0100

    feat(zero): external paths, update names

--- bin/zero/cli/src/command.rs
+++ bin/zero/cli/src/command.rs
@@ -24,7 +24,7 @@ use sc_service::PartialComponents;
 
 impl SubstrateCli for Cli {
 	fn impl_name() -> String {
-		"Substrate Node".into()
+		"ZERO Node".into()
 	}
 
 	fn impl_version() -> String {
@@ -40,7 +40,7 @@ impl SubstrateCli for Cli {
 	}
 
 	fn support_url() -> String {
-		"https://github.com/paritytech/substrate/issues/new".into()
+		"https://github.com/playzero/zero-network/issues/new".into()
 	}
 
 	fn copyright_start_year() -> i32 {
@@ -56,7 +56,7 @@ impl SubstrateCli for Cli {
 				),
 			"dev" => Box::new(chain_spec::development_config()),
 			"local" => Box::new(chain_spec::local_testnet_config()),
-			"fir" | "flaming-fir" => Box::new(chain_spec::flaming_fir_config()?),
+			"subzero" | "alphaville" => Box::new(chain_spec::subzero_config()?),
 			"staging" => Box::new(chain_spec::staging_testnet_config()),
 			path =>
 				Box::new(chain_spec::ChainSpec::from_json_file(std::path::PathBuf::from(path))?),
