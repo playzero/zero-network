c4544501e8403118d53f9e4eab5ea131e68ea065
vasylenko-yevhen <ggbigkon@gmail.com>
2022-07-19 20:58:19 +0300

    Update default parachain spec values and refresh binary files.

--- bin/zero-parachain/runtime/src/lib.rs
+++ bin/zero-parachain/runtime/src/lib.rs
@@ -255,7 +255,7 @@ parameter_types! {
 		})
 		.avg_block_initialization(AVERAGE_ON_INITIALIZE_RATIO)
 		.build_or_panic();
-	pub const SS58Prefix: u16 = 42;
+	pub const SS58Prefix: u16 = 25;
 }
 
 // Configure FRAME pallets to include in runtime.
