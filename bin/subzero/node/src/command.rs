3c52c09517697414da65b1ca1e2fe0f4bcb1b3d3
vasylenko-yevhen <ggbigkon@gmail.com>
2023-02-14 18:13:33 +0200

    Updated Dockerfiles to use valid rust version.

--- bin/subzero/node/src/command.rs
+++ bin/subzero/node/src/command.rs
@@ -24,7 +24,8 @@ fn load_spec(id: &str) -> std::result::Result<Box<dyn ChainSpec>, String> {
 	Ok(match id {
 		"dev" => Box::new(chain_spec::development_config()),
 		"template-rococo" => Box::new(chain_spec::local_testnet_config()),
-		"" | "local" => Box::new(chain_spec::local_testnet_config()),
+		"local" => Box::new(chain_spec::local_testnet_config()),
+		"" | "subzero" => Box::new(chain_spec::subzero_config()?),
 		path => Box::new(chain_spec::ChainSpec::from_json_file(std::path::PathBuf::from(path))?),
 	})
 }
