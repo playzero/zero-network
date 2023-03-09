986ca98ad0e92b61e6bc78a65066105f01bbbecb
FiberMan <filimonchuk@gmail.com>
2023-03-09 14:41:42 +0100

    updates chain to v72, uses latest gamedao release-1.3.0

--- bin/subzero-dev/runtime/src/lib.rs
+++ bin/subzero-dev/runtime/src/lib.rs
@@ -167,7 +167,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("subzero"),
 	impl_name: create_runtime_str!("dev"),
 	authoring_version: 75,
-	spec_version: 71,
+	spec_version: 72,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
